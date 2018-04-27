33a0149377883ee3b22a87ae203148da3192d783
thibaultrousset <thibaultrousset94@gmail.com>
2018-04-27 14:22:52 +0200

    V5

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+
+
+function animateValue(id, start, end, duration) {
+    var range = end - start;
+    var current = start;
+    var increment = end > start? 1 : -1;
+    var stepTime = Math.abs(Math.floor(duration / range));
+    var obj = document.getElementById("value");
+    var timer = setInterval(function() {
+        current += increment;
+        obj.innerHTML = current;
+        if (current == end) {
+            clearInterval(timer);
+        }
+    }, stepTime);
+}
+
+animateValue("value", 100, 25, 2000);
