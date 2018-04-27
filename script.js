c76e19f2a0b8ef76d1cb6e43ed6a0a2c79aacc1a
thibaultrousset <thibaultrousset94@gmail.com>
2018-04-27 17:10:12 +0200

    V6

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
+var a = 0;
+$(window).scroll(function() {
 
+  var oTop = $('#counter').offset().top - window.innerHeight;
+  if (a == 0 && $(window).scrollTop() > oTop) {
+    $('.counter-value').each(function() {
+      var $this = $(this),
+        countTo = $this.attr('data-count');
+      $({
+        countNum: $this.text()
+      }).animate({
+          countNum: countTo
+        },
 
+        {
 
+          duration: 2000,
+          easing: 'swing',
+          step: function() {
+            $this.text(Math.floor(this.countNum));
+          },
+          complete: function() {
+            $this.text(this.countNum);
+            //alert('finished');
+          }
 
-function animateValue(id, start, end, duration) {
-    var range = end - start;
-    var current = start;
-    var increment = end > start? 1 : -1;
-    var stepTime = Math.abs(Math.floor(duration / range));
-    var obj = document.getElementById("value");
-    var timer = setInterval(function() {
-        current += increment;
-        obj.innerHTML = current;
-        if (current == end) {
-            clearInterval(timer);
-        }
-    }, stepTime);
-}
+        });
+    });
+    a = 1;
+  }
 
-animateValue("value", 100, 25, 2000);
+});
